a279e47ef352a472942eadabd0fd999b5d08701a
Aziza H <mess.aziza@gmail.com>
2023-03-01 17:18:50 +0100

    creation du footer et header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './style/index.scss';
 //import Error from './pages/error/Error';
-import Home from './pages/home/Home';
+//import Home from './pages/home/Home';
+//import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
 function App() {
   return (
     <div className="App">
 
-      <Home />
+      <Header />
+
     </div>
   );
 }
