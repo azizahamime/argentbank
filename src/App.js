907cfa069fb0d617bd7db61151b40efb93712852
Aziza H <mess.aziza@gmail.com>
2023-03-01 16:49:42 +0100

    page error et home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './style/index.scss';
+//import Error from './pages/error/Error';
+import Home from './pages/home/Home';
 function App() {
   return (
     <div className="App">
-      hello
+
+      <Home />
     </div>
   );
 }
