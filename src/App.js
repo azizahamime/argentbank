ed07f9700985e8e6fcb3c914fcb2ff27fd1f179f
Aziza H <mess.aziza@gmail.com>
2023-03-02 09:08:47 +0100

    fontawsome , routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './style/index.scss';
-//import Error from './pages/error/Error';
-//import Home from './pages/home/Home';
-//import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Error from "./pages/error/Error";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-
+    <Router>
       <Header />
-
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p className="footer-text">Copyright 2020 Argent Bank</p>
+            <p className="footer-text">Copyright &copy; 2020, Argent Bank</p>
         </footer>
     );
 }
