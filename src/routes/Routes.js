72e862a0400c38806fd0b726727ec7805d2278c0
Aziza H <mess.aziza@gmail.com>
2023-05-01 09:27:38 +0200

    api + private Routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,34 @@
+import '../style/index.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Error from "../pages/error/Error";
+import Home from "../pages/home/Home";
+import Login from '../pages/login/Login';
+import Profil from '../pages/Profil/Profil';
+import PrivateRoute from './PrivateRoutes';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={
+          <PrivateRoute>
+            <Profil />
+          </PrivateRoute>
+        }
+        />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
